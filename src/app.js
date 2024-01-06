98cf90a1d23c534dbd1b39c8e829c77f64f5819f
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-06 21:52:48 +0530

    changed

--- src/app.js
+++ src/app.js
@@ -5,10 +5,28 @@ const app = express();
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
-//Middlewares
+// Middlewares
 app.use(express.json());
 
-// GET endpoint for sending the products to client by id
-//// Endpoint - /api/v1/products/:id
+// Write GET endpoint for sending all the products to client here
+// Endpoint - /api/v1/products
+app.get("/api/v1/products" , (req,res)=>{
+if(products.length) {
+res.status(200).json({
+  status: "success",
+  message:"Product fetched successfully",
+   data: {
+        products: products.map(product => ({
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          quantity: product.quantity
+        }))
+      }
+});
+}else{
+  res.status(404).json({message:"Product not found"});
+}
+})
 
 module.exports = app;
