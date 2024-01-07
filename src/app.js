1c502168fb561aa750e602c6142a01f3e1f8fd76
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-07 11:58:19 +0530

    changed

--- src/app.js
+++ src/app.js
@@ -5,28 +5,38 @@ const app = express();
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
-// Middlewares
+//Middlewares
 app.use(express.json());
 
-// Write GET endpoint for sending all the products to client here
-// Endpoint - /api/v1/products
-app.get("/api/v1/products" , (req,res)=>{
-if(products.length) {
-res.status(200).json({
-  status: "success",
-  message:"Product fetched successfully",
-   data: {
-        products: products.map(product => ({
-          id: product.id,
-          name: product.name,
-          price: product.price,
-          quantity: product.quantity
-        }))
-      }
-});
-}else{
-  res.status(404).json({message:"Product not found"});
-}
+// GET endpoint for sending the products to client by id
+//// Endpoint - /api/v1/products/:id
+app.get('/api/v1/products/:id' ,(req,res)=>{
+    let {id}=req.params;
+    id*=1;
+    if(!isNaN(id)){
+        const product = products.find((product)=>product.id===id);
+        if(!product){
+            res.status(404).json({
+                status:"failed",
+                message:"Product not found",
+            
+            })
+        }else{
+            res.status(200).json({
+                status:"success",
+                message:"Product fetched successfully",
+                data:{
+                    product,
+                }
+            })
+        }
+    }else{
+        res.status(400).json({
+            status:"failed",
+            message:"bad request",
+        
+        })
+    }
 })
 
 module.exports = app;
